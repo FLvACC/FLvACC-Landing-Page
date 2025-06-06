ae763c6921386051ee1c8ddcc6a919b8c79bc5f7
FLvACC <flightline.atcprogram@gmail.com>
2025-06-06 06:11:36 +0100

    Fixes
    - When the website loads, start by fading in the "Your dream" and the flightLine logo with the same timing
    - Make the "experience how to" bla bla fade in when the logo is shrinking to the top
    - The FLvACC logo text "FL" and "vACC" isn't the same font size when it shrunk and looks slightly goofy
    - Make the background gray instead of black
    - Make the shrink animations all smooth out but not smooth in so it looks like it jumps away

--- index.js
+++ index.js
@@ -1,49 +1,63 @@
 //Animations on load
+const dream = document.getElementById('dream');
 const flightline = document.getElementById('flightline');
 const ourRunway = document.getElementById('ourRunway');
 const vaccOnly = document.getElementById('vaccOnly');
 const vaccBackground = document.getElementById('vaccBackground');
 const flOnly = document.getElementById('flOnly');
-flightline.addEventListener('transitionend', () => {
-    vaccOnly.style.opacity = 1;
-    vaccBackground.style.transform = 'translate(64%, -90%)';
-    vaccBackground.style.right = '50%';
-    ourRunway.style.opacity = 1; 
-    flOnly.style.left = 'max(30px, 4.5vh)';
-    flOnly.style.top = 'max(10px, 1.5vh)';
+dream.addEventListener('transitionend', () => {
+    flOnly.style.opacity = 1;
+    flightline.addEventListener('transitionend', () => {
+        vaccOnly.style.opacity = 1;
+        vaccBackground.style.transform = 'translate(64%, -90%)';
+        vaccBackground.style.right = '50%';
+        ourRunway.style.opacity = 1; 
+        flOnly.style.left = '35px';
+        flOnly.style.top = 'max(10px, 1.5vh)';
+    });
+    setTimeout(() => {
+        flightline.style.opacity = 0;
+        flightline.style.transition = 'opacity 0.75s ease-out';
+    }, 50);
 });
-flightline.style.opacity = 0;
+dream.style.opacity = 1;
+flightline.style.opacity = 1;
 const topContainer = document.getElementById('top');
 const nav = document.getElementById('nav');
 const navItems = document.querySelectorAll('.navItem');
 const signIn = document.getElementById('signIn');
 const downArrow = document.getElementById('downArrow');
 const learnMore = document.getElementById('learnMore');
-flOnly.addEventListener('transitionend', () => {
+const promoMessage = document.getElementById('promoMessage');
+ourRunway.addEventListener('transitionend', () => {
     //Lock #flightlineDream elements in place
     flOnly.style.top = '10px';
-    flOnly.style.left = '30px';
-    setTimeout(() => {flOnly.style.transition = 'none'}, 750); //Allows transition to final locked position
+    flOnly.style.left = '35px';
     vaccOnly.style.fontSize = '70px';
+    vaccBackground.style.height = '68px';
+    document.getElementById('flDreamLine1').style.height = '68px';
     document.getElementById('dream').style.fontSize = '27.7px';
     ourRunway.style.fontSize = '27.7px';
-    //Transition #top
+    setTimeout(() => {flOnly.style.transition = 'none'}, 750); //Allows transition to final locked position
+    //Transition #top & #promoMessage 
     topContainer.style.height = '75vh';
-    topContainer.style.backgroundColor = 'rgba(0,0,0,0.2)';
+    topContainer.style.animation = 'topAnimation 1s forwards ease-out';
+    setTimeout(() => {topContainer.style.backgroundImage = 'url(media/plane.png)'}, 400);
+    promoMessage.style.opacity = 1;
     //After transition of #top
     setTimeout(() => {
-        topContainer.style.transition = 'none'
+        //topContainer.style.transition = 'none';
         downArrow.style.opacity = 1;
         learnMore.style.opacity = 1;
-    }, 750);
+    }, 1250);
     //Transition #nav (has 0.25s delay)
     nav.style.transform = 'skew(20deg) translateX(0)';
     var delay = 0.2;
     navItems.forEach(item => {
-        item.style.transition = `opacity 0.75s ease-in-out ${delay}s`;
+        item.style.transition = `opacity 0.75s ease-out ${delay}s`;
         item.style.opacity = 1;
         delay+=0.2;
     });
-    signIn.style.transition = `opacity 0.75s ease-in-out ${delay}s`
+    signIn.style.transition = `opacity 0.75s ease-out ${delay}s`
     signIn.style.opacity = 1;
 });
